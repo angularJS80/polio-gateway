8d73c463296def64a9a94af07a15e31a70a03a1f
yongbeom.cho <unbone80@gmail.com>
2025-06-04 10:54:53 +0900

    feat : 키클락 속성을 정의하고 이용하도록 변경

---
+++ src/main/java/com/polio/gateway/infrastructure/keycloak/prop/KeycloakSecurityProperties.java
@@ -0,0 +1,16 @@
+package com.polio.gateway.infrastructure.keycloak.prop;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+@Data
+@ConfigurationProperties(prefix = "keycloak")
+public class KeycloakSecurityProperties {
+    private String serverUrl;
+    private String realm;
+    private String clientId;
+    private String clientSecret;
+    private String username;
+    private String password;
+}
