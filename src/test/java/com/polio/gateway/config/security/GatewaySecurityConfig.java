f836e035fd850b09fa07e93e7535fd22b767e171
yongbeom.cho <unbone80@gmail.com>
2025-06-04 10:21:23 +0900

    feat : keycloak 속성 정의 , oauth2 리소스 속성정의, 롬복 의존성 추가

---
+++ src/test/java/com/polio/gateway/config/security/GatewaySecurityConfig.java
@@ -0,0 +1,25 @@
+package com.polio.gateway.config.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
+public class GatewaySecurityConfig {
+
+    @Bean
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+        http
+            .csrf(csrf -> csrf.disable())
+            .authorizeExchange(exchanges -> exchanges
+                .pathMatchers("/api/polio/**").authenticated()  // 보호할 경로 설정
+                .anyExchange().permitAll()
+            )
+            .oauth2ResourceServer(oauth2 -> oauth2.jwt());
+
+        return http.build();
+    }
+}
